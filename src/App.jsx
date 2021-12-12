18f37935d60087c4ac654c71f9ca9433d6effb6e
Manav Misra <manavm@visionwebsoft.com>
2021-12-11 19:34:10 -0600

    :building_construction: useData

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,16 @@
-import ky from "ky";
-import React from "react";
-import List from "./List";
-
-const url = "http://hn.algolia.com/api/v1/search?query=react";
-
-const reducer = (state, { type, payload }) => {
-  switch (type) {
-    case "SET_DATA":
-      return {
-        ...state,
-        data: payload,
-        error: null,
-      };
-    case "SET_ERROR":
-      return {
-        ...state,
-        error: payload,
-      };
-    default:
-      return state;
-  }
-};
+import List from "./components/List";
+import useData from "./hooks/useData";
 
 function App() {
-  const [state, dispatch] = React.useReducer(reducer, {});
-
-  React.useEffect(() => {
-    ky.get(url)
-      .json()
-      .then((data) => {
-        dispatch({ type: "SET_DATA", payload: data });
-      })
-      .catch((error) => {
-        dispatch({ type: "SET_ERROR", payload: error.message });
-      });
-  }, []);
-
-  if (state.error) {
-    return <p className="text-red-500">{state.error}</p>;
+  const data = useData();
+  if (data.error) {
+    return <p className="text-center text-red-500">{data.error}</p>;
   }
 
-  return state.data ? (
-    <List items={state.data.hits} />
+  return data.data ? (
+    <List items={data.data.hits} />
   ) : (
-    <p className="text-yellow-500">Loading...</p>
+    <p className="text-center text-yellow-500">Loading...</p>
   );
 }
 
---
+++ src/hooks/useData.jsx
@@ -0,0 +1,41 @@
+import ky from "ky";
+import React from "react";
+
+const URL = "http://hn.algolia.com/api/v1/search?query=react";
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case "SET_DATA":
+      return {
+        ...state,
+        data: payload,
+        error: null,
+      };
+    case "SET_ERROR":
+      return {
+        ...state,
+        error: payload,
+      };
+    default:
+      return state;
+  }
+};
+
+function useData() {
+  const [state, dispatch] = React.useReducer(reducer, {});
+
+  React.useEffect(() => {
+    ky.get(URL)
+      .json()
+      .then((data) => {
+        dispatch({ type: "SET_DATA", payload: data });
+      })
+      .catch((error) => {
+        dispatch({ type: "SET_ERROR", payload: error.message });
+      });
+  }, []);
+
+  return state;
+}
+
+export default useData;

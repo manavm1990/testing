f3aef1403781934569acfbadbca5129db3b5d5e8
Manav Misra <manavm@visionwebsoft.com>
2021-12-11 21:34:14 -0600

    :white_check_mark: Snapshot test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./App";
+
 describe("My Test Suite", () => {
   it("My Test Case", () => {
     expect(true).toBe(true);
+  });
+});
+
+describe("Counter", () => {
+  test("snapshot renders", () => {
+    const component = renderer.create(<App />);
+    expect(component.toJSON()).toMatchSnapshot();
   });
 });
